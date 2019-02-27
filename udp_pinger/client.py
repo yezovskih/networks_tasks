15d0e51a72ec66d677d8a7576ba45e83feb0e328
yezovskih <jezovskih@gmail.com>
2019-02-27 17:44:43 +0200

    init udp pinger

---
+++ udp_pinger/client.py
@@ -0,0 +1,24 @@
+from socket import *
+from time import time
+
+serverAddress = ('', 12000)
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.settimeout(1)
+
+for messageCounter in range(1, 10):
+    now = time()
+
+    message = 'Ping ' + messageCounter + ' ' + now
+    print message
+
+    try:
+        clientSocket.sendto(message, serverAddress)
+        modifiedMessage, addr = clientSocket.recvfrom(1024)
+        print modifiedMessage
+
+        rtt_time = time() - now
+        print 'RTT time: ', rtt_time
+    except socket.timeout as err:
+        print 'Request timed out'
+
+clientSocket.close()
